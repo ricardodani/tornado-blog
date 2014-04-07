0cf280ea7c57c8a3d155b17a26d5b734c1971e1c
Ricardo L. Dani <ricardodani@gmail.com>
2014-04-07 16:53:01 -0300

    Added base handler and doing correct logic on MainHandler.

--- blog/handlers.py
+++ blog/handlers.py
@@ -3,22 +3,28 @@
 import tornado
 from models import Post
 
-class MainHandler(tornado.web.RequestHandler):
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+
+class MainHandler(BaseHandler):
     def get(self):
-        posts = self.application.db.query(Post).all()
-        self.write("Main blog {}!".format(len(posts)))
+        self.render('home.html', posts=Post.get_posts(self.db))
 
 
-class AddPostHandler(tornado.web.RequestHandler):
+class AddPostHandler(BaseHandler):
     def get(self):
         self.write("Add post")
 
 
-class PostHandler(tornado.web.RequestHandler):
+class PostHandler(BaseHandler):
     def get(self, post_id):
         self.write("Post {}".format(post_id))
 
 
-class PostCommentsHandler(tornado.web.RequestHandler):
+class PostCommentsHandler(BaseHandler):
     def get(self, post_id):
         self.write("Post {} comments".format(post_id))
